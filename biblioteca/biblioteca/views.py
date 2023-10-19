16f377957cbf7d71b43f0ad78c512391d9b39212
Alvaro Rodriguez <alvaro.rodriguez.garrido.al@iespoligonosur.org>
2023-10-19 12:41:02 +0200

    urls subidas

--- biblioteca/biblioteca/views.py
+++ biblioteca/biblioteca/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from biblioteca.models import Libro
+from biblioteca.models import Libro, Cliente, Biblioteca
+from django.db.models import Q
 
 # Create your views here.
 
@@ -10,10 +11,27 @@ def listar_libros(request):
     libros = Libro.objects.select_related("bliblioteca").prefetch_related("autores")
     return render(request, "libro/lista.html", {"libros_mostrar":libros})
 
+def dame_libros_fecha(request, anyo_libro, mes_libro):
+    libros = Libro.objects.select_related("bliblioteca").prefetch_related("autores")
+    libros = libros.filter(fecha_publicacion__year = anyo_libro, fecha_publicacion__month = mes_libro)
+    return render(request, "libro/lista.html", {"libros_mostrar":libros})
+
+def dame_libros_idioma(request, idioma):
+    libros = Libro.objects.select_related("bliblioteca").prefetch_related("autores")
+    libros = libros.filter(Q(idioma=idioma)|Q(idioma="ES")).order_by("fecha_publicacion")
+    return render(request, "libro/lista.html", {"libros_mostrar":libros})
+
 def dame_libro(request, id_libro):
     libro = Libro.objects.select_related("bliblioteca").prefetch_related("autores").get(id=id_libro)
     return render(request, "libro/libro.html",{"libro":libro})
 
+
+
 def listar_clientes(request):
-    clientes = Cliente.objets.all()
-    return render(request, "cliente/clientelist.html", {"cliente_mostrar":clientes})
+    clientes = Cliente.objects.all()
+    return render(request, "cliente/clientelist.html", {"clientes_mostrar":clientes})
+
+def listar_biblioteca(request):
+    bibliotecas = Biblioteca.objects.all()
+    return render(request, "biblioteca/bibliotecalist.html", {"biblioteca_mostrar":bibliotecas})
+
